0dc6c98a6849053a7be30ea88e332777b2710510
Ivens Diego Müller <ivens.muller@amazingit.com.br>
2020-01-15 11:02:26 -0300

    FrontEnd Web Done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-w0yno.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
